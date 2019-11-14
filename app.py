9e004e66cee9da414779c082069aec09f986b694
Jaeseung Park <56590313+zeseung@users.noreply.github.com>
2019-11-14 22:13:07 +0900

    북마크 및 코멘트 등록 완료
    Robo 3T로 동작 확인까지 하였습니다. 
    - 북마크 등록 : 이미지 불러오기 부분은 테스트 전 입니다.
    - 북마크를 등록할 때 '좋아요' 카운트(likeCount)를 0으로 올려주고, 북마크별 좋아요를 하면 +1씩 카운트하려고 합니다. 해당 기능은 웹 개발쪽에서의 작업 영역일까요?

--- app.py
+++ app.py
@@ -32,8 +32,11 @@ def bookmarking():
     category_receive = request.form['category_give']
     likeCount_receive = request.form['likeCount_give']
     title_receive = request.form['title_give']
+    url_receive = request.form['url_give']
 
 # meta tag를 스크래핑 하는 부분 (이미지만 가져오게 될 듯 // 이미지가 없는 경우 대체 이미지 노출 - 이건 자바스크립트로 하는 건가?)
+
+    '''
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
     data = requests.get(url_receive, headers=headers)
 
@@ -44,20 +47,22 @@ def bookmarking():
 
 # mongoDB에 넣는 부분
     bookmark = {'category': category_receive, 'likeCount': likeCount_receive, 'title': title_receive, 'image': url_image}
-    db.bookmarkDB.insert_one(bookmark)
+    '''
+    bookmark = {'category': category_receive, 'likeCount': likeCount_receive, 'title': title_receive, 'url': url_receive}
+    db.bookmarkDB_test.insert_one(bookmark)
 
     return jsonify({'result': 'success'})
 
 @app.route('/comment', methods=['POST'])
 def commenting():
 # 코멘트를 남기면 북마크 별로 저장 (mongoDB 고유 아이디는 어떻게 쌓지?)
-    bookmarkId_receive = request.form['_id']
+    bookmarkId_receive = request.form['bookmarkId_give']
     nickname_receive = request.form['nickname_give']
     contents_receive = request.form['contents_give']
 
 #mongoDB에 넣는 부분
     comment = {'bookmarkId': bookmarkId_receive, 'nickname': nickname_receive, 'contents': contents_receive}
-    db.commentDB.insert_one(comment)
+    db.commentDB_test.insert_one(comment)
 
     return jsonify({'result': 'success'})
 '''
